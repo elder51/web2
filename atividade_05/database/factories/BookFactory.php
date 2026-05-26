9562a2ddadbb7a3deb01677fb659fb664f914fe5
elder51 <elder2051@gmail.com>
2026-05-25 21:32:03 -0300

    atividade-5.1 web2

---
+++ atividade_05/database/factories/BookFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Models\Publisher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BookFactory extends Factory
+{
+    protected $model = Book::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'author_id' => Author::factory(),
+            'category_id' => Category::factory(),
+            'publisher_id' => Publisher::factory(),
+            'published_year' => $this->faker->year,
+            'pages' => $this->faker->numberBetween(100, 800)
+        ];
+    }
+}
