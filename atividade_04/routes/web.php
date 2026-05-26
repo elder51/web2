2e7f484dfe0c4bdb0b7e870d7a313a95f52f0c87
elder51 <elder2051@gmail.com>
2026-05-25 21:13:50 -0300

    atividade-4.3 web2

--- atividade_04/routes/web.php
+++ atividade_04/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PublisherController;
+use App\Http\Controllers\BookController;
 
 
 Route::get('/', function () {
@@ -18,3 +19,10 @@ Route::resource('categories', CategoryController::class);
 Route::resource('authors', AuthorController::class);
 Route::resource('publishers', PublisherController::class);
 
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+Route::resource('books', BookController::class)->except(['create', 'store']);
